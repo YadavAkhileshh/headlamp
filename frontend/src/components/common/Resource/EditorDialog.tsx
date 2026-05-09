a603e4ef8a8a6597e698586f47e35316941a69c6
YadavAkhileshh <yadavakhil2501@gmail.com>
2026-05-10 00:54:52 +0530

    frontend: EditorDialog: Revert trimStart and keep only onUndo fixes for PR 5450

--- frontend/src/components/common/Resource/EditorDialog.tsx
+++ frontend/src/components/common/Resource/EditorDialog.tsx
@@ -203,7 +203,7 @@ export default function EditorDialog(props: EditorDialogProps) {
   }
 
   function looksLikeJson(code: string) {
-    const trimmedCode = code.trimStart();
+    const trimmedCode = code.trimLeft();
     const firstChar = !!trimmedCode ? trimmedCode[0] : '';
     if (['{', '['].includes(firstChar)) {
       return true;
